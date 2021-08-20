4c24be9242ab0a6a3c424641292acf7397f50cc6
Caseywalker <casey.walker118@gmail.com>
2021-08-19 20:15:27 -0500

    Add project files.

---
+++ PizzaToppings/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PizzaToppings
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      List<Pizza> pizzaCollection = new List<Pizza>();
+      var jsonFile = File.ReadAllText(@"C:\Users\16159\source\repos\PizzaToppings\PizzaToppings\pizzas.json");
+
+      pizzaCollection = JsonConvert.DeserializeObject<List<Pizza>>(File.ReadAllText(@"c:\Users\16159\source\repos\PizzaToppings\PizzaToppings\pizzas.json"));
+
+      var results = pizzaCollection.GroupBy(pizza => pizza.Toppings).OrderBy()
+
+      foreach (var uniquePizza in results)
+      {
+        var test = "Pizza Toppings";
+        foreach (var topping in uniquePizza.Key)
+        {
+          test += $" {topping}";
+        }
+        Console.WriteLine(test);
+      } 
+    }
+  }
+}
